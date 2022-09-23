e07835dbc3efcf4c2e51fb5883e4acd115eb9677
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-09-23 18:32:11 +0700

    casuarina: Build more packages

--- device.mk
+++ device.mk
@@ -46,20 +46,33 @@ PRODUCT_VENDOR_PROPERTIES += ro.soc.model=SDM450
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl:64 \
+    android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.1-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.frameworks.displayservice@1.0 \
+
+PRODUCT_PACKAGES += \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
+    libgralloc.qti \
     libqdMetaData \
+    libqdMetaData.system \
+    libqdMetaData.vendor \
     libvulkan \
     libtinyxml \
-    vendor.display.config@1.0.vendor
+    libtinyxml.vendor
+
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@2.0.vendor
 
 # Disable skip validate
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -115,25 +128,27 @@ PRODUCT_PACKAGES += \
     init.msm.usb.configfs.rc \
     ueventd.qcom.rc \
     vold.fstab \
-    init.qti.ims.sh \
-    init.qcom.coex.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
-    init.qcom.sh \
-    init.qcom.class_core.sh \
-    init.class_main.sh \
-    init.class_late.sh \
-    init.qcom.usb.sh \
-    init.qcom.efs.sync.sh \
-    qca6234-service.sh \
-    init.mdm.sh \
-    init.qcom.sensors.sh \
-    init.qcom.crashdata.sh \
-    init.qti.can.sh \
-    init.qti.charger.sh \
     init.time_daemon.rc \
     elan_device.rc
+
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.class_late.sh \
+    init.mdm.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.can.sh \
+    init.qti.ims.sh \
+    init.qti.charger.sh \
+    qca6234-service.sh \
 
 # CRDA += init.crda.sh #TODO fix CRDA packages later
 
@@ -145,14 +160,12 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl:32 \
-    libbson.vendor \
-    libxml2
+    libgui_vendor \
+    libstdc++.vendor
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.isp.clock.optmz=0 \
