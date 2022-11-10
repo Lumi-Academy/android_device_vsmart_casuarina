7475054c9834480611cfdac6e7a8a7b971a6b3c1
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-11-10 13:04:16 +0700

    casuarina: Import fw services from stock

--- device.mk
+++ device.mk
@@ -200,13 +200,6 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1-service
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/biometrics/,$(TARGET_COPY_OUT_VENDOR)/bin/hw)
-
-PRODUCT_PACKAGES += \
     LumiOverlay
 
 # FBE support
