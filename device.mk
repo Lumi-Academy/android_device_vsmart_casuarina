a06654e953351ab9a96ee6ca99bd8eaffe233395
HoangLong-Lumi <hoanglong.lumi404@gmail.com>
2023-03-04 18:29:54 +0700

    casuarina: Cleanup unused Bluetooth packages
    Signed-off-by: HoangLong-Lumi <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -99,8 +99,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0 \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0 \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
